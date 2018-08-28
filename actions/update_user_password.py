36dc65642884607b49726b533c4e256c940c9b6a
bigmstone <matthew99@gmail.com>
2018-08-28 02:40:29 -0500

    Adding password update

---
+++ actions/update_user_password.py
@@ -0,0 +1,17 @@
+from azure.graphrbac.models import UserCreateParameters, PasswordProfile
+
+from lib.base import AzureBaseADUserPassAction
+
+
+class AzureCreateVMAction(AzureBaseADUserPassAction):
+    def run(self, user_object_id, password, force_password_change):
+        self.graphrbac_client.users.update(
+            user_object_id,
+            {
+                'passwordProfile': PasswordProfile(
+                    password=password,
+                    force_change_password_next_login=force_password_change
+                )
+            }
+        )
+        return {"updated": True}
