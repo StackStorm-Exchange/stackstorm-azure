8ded4bb5f4ced3db5d9fd9a55da80ce0b58de713
bigmstone <matthew99@gmail.com>
2018-08-28 02:29:17 -0500

    Adding enable/disable user account

---
+++ actions/user_enable.py
@@ -0,0 +1,9 @@
+from azure.graphrbac.models import UserCreateParameters, PasswordProfile
+
+from lib.base import AzureBaseADUserPassAction
+
+
+class AzureCreateVMAction(AzureBaseADUserPassAction):
+    def run(self, user_object_id, enable):
+        self.graphrbac_client.users.update(user_object_id, {'accountEnabled': enable})
+        return {"updated": True}
