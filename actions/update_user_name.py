6e829da87ceda56fe787f09bfedb770ce526b3cb
bigmstone <matthew99@gmail.com>
2018-08-28 02:21:19 -0500

    Adding update name

---
+++ actions/update_user_name.py
@@ -0,0 +1,20 @@
+from azure.graphrbac.models import UserCreateParameters, PasswordProfile
+
+from lib.base import AzureBaseADAction
+
+
+class AzureCreateVMAction(AzureBaseADAction):
+    def run(self, user_object_id, first_name, last_name):
+        if not last_name and not first_name:
+            return {"updated": False}
+
+        update = {}
+
+        if first_name:
+            update['givenName'] = first_name
+
+        if last_name:
+            update['surName'] = last_name
+
+        self.graphrbac_client.users.update(user_object_id, update)
+        return {"updated": True}
