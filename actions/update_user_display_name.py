89cf610ab8ce41fa1653437f2ecb3ee00c129260
bigmstone <matthew99@gmail.com>
2018-08-28 02:10:32 -0500

    Adding update display name

---
+++ actions/update_user_display_name.py
@@ -0,0 +1,9 @@
+from azure.graphrbac.models import UserCreateParameters, PasswordProfile
+
+from lib.base import AzureBaseADAction
+
+
+class AzureCreateVMAction(AzureBaseADAction):
+    def run(self, user_object_id, display_name):
+        self.graphrbac_client.users.update(user_object_id, {'displayName': display_name})
+        return {"updated": True}
